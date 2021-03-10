33ef673c51bf0fed2b3df1c3829269436e1414ef
Tom Feron <tho.feron@gmail.com>
2021-03-10 10:31:57 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 for f in $(ls -a .); do
   if [ "$f" == "install.sh" ] || [ "$f" == "." ] || [ "$f" == ".." ]; then
-    # Do nothing
+    true # Do nothing
   elif [ -d "$f" ]; then
     mkdir -p "$HOME/$f"
   else
