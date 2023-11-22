9f6971c946b89f6d061953a5da014a2a6297b6de
Tom Feron <tho.feron@gmail.com>
2023-11-22 19:50:49 +0000

    Vim completions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,6 +29,16 @@ local plugins = {
     "ntpeters/vim-better-whitespace",
     "folke/which-key.nvim",
     "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp",
+    {
+      "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp"
+    },
+    "saadparwaiz1/cmp_luasnip",
   }
 }
 
@@ -82,7 +92,57 @@ require("telescope").setup {
 require("which-key").setup {
 }
 
-require("lspconfig").rnix.setup {}
+local cmp = require("cmp")
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+    { name = "path" },
+  }),
+}
+
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" }
+  }, {
+    {
+      name = "cmdline",
+      option = {
+        ignore_cmds = { "Man", "!" }
+      }
+    }
+  })
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").rnix.setup {
+  capabilities = capabilities
+}
 
 -- Need to be before the other options.
 vim.opt.compatible = false
@@ -92,7 +152,7 @@ vim.opt.filetype.plugin = "on"
 vim.opt.filetype.indent = "on"
 
 vim.opt.number = true
-vim.opt.numberwidth = 3
+vim.opt.numberwidth = 4
 
 vim.opt.undofile = true
 
