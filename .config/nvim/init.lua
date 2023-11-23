c010d7baac5eb7e60712693c4918d924d97da80c
Tom Feron <tho.feron@gmail.com>
2023-11-23 08:43:50 +0000

    Vim LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -140,9 +140,30 @@ cmp.setup.cmdline(':', {
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").rnix.setup {
-  capabilities = capabilities
-}
+local lsp_attach = function()
+  print("hello")
+  local nmap = function(key, func)
+    print("setting keymap")
+    vim.keymap.set("n", key, func, { buffer = 0 })
+  end
+  nmap("K", vim.lsp.buf.hover)
+  nmap("gd", vim.lsp.buf.definition) -- <C-t> to jump back.
+  nmap("gr", vim.lsp.buf.references)
+  nmap("gt", vim.lsp.buf.type_definition)
+  nmap("gi", vim.lsp.buf.implementation)
+  nmap("<leader>df", vim.diagnostic.goto_next)
+  nmap("<leader>db", vim.diagnostic.goto_prev)
+  nmap("<leader>.", vim.lsp.buf.code_action)
+  nmap("<leader>r", vim.lsp.buf.rename)
+end
+
+local setup_lsp = function(name, args)
+  args.capabilities = capabilities
+  args.on_attach = lsp_attach
+  require("lspconfig")[name].setup(args)
+end
+
+setup_lsp("rnix", {})
 
 -- Need to be before the other options.
 vim.opt.compatible = false
